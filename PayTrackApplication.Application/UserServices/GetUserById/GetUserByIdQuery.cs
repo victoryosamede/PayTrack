fcd207eefa3f02c31e76b95c6770ae4e43ec14b2
Victory <uvictory0@gmail.com>
2023-10-20 16:18:23 +0100

    Added Get By Id Query and Query Handler to the User Service

---
+++ PayTrackApplication.Application/UserServices/GetUserById/GetUserByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using PayTrackApplication.Application.CQRS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayTrackApplication.Application.UserServices.GetUserById
+{
+    public class GetUserByIdQuery: QueryBase
+    {
+
+    }
+}
--- PayTrackApplication.Application/UserServices/GetUserById/GetUserByIdQueryHandler.cs
+++ PayTrackApplication.Application/UserServices/GetUserById/GetUserByIdQueryHandler.cs
@@ -1,30 +1,30 @@
 ﻿using MediatR;
 using PayTrackApplication.Application.Services.AuthenticationServices;
 using PayTrackApplication.Application.Services.PayTrackServices;
-using PayTrackApplication.Domain.Models.UsersFolder;
+using PayTrackApplication.Application.UserServices.GetAllUsers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PayTrackApplication.Application.UserServices.GetAllUsers
+namespace PayTrackApplication.Application.UserServices.GetUserById
 {
-    internal class GetAllUsersQueryHandler: IRequestHandler<GetAllUsersQuery, ActionResponse> 
+    internal class GetUserByIdQueryHandler: IRequestHandler<GetUserByIdQuery, ActionResponse>
     {
         private readonly IUserRepository _UserRepo;
         private readonly IAuthenticationManager _authenticationManager;
-        public GetAllUsersQueryHandler(IUserRepository userRepo, IAuthenticationManager authenticationManager)
+        public GetUserByIdQueryHandler(IUserRepository userRepo, IAuthenticationManager authenticationManager)
         {
             _UserRepo = userRepo;
-             
+
             _authenticationManager = authenticationManager;
         }
-        public async Task<ActionResponse> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+        public async Task<ActionResponse> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
         {
             return new ActionResponse
             {
-                PayLoad = await _UserRepo.GetAll()
+                PayLoad = await _UserRepo.GetById(request.Id)
             };
         }
     }
